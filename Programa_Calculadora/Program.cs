7f46d0efbef1816b05b582f856b55db25321b32c
naiaragabriela <naigabi31@gmail.com>
2023-03-03 15:09:06 -0300

    Add project files.

---
+++ Programa_Calculadora/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float n1, n2;
+
+        Console.WriteLine("Digite o primeiro número para a calculadora realizar as operações");
+        n1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o primeiro número para a calculadora realizar as operações");
+        n2= float.Parse(Console.ReadLine());
+
+        Console.WriteLine("A soma dos números digitados é: " + (n1 + n2));
+        Console.WriteLine("A subtração dos números digitados é: " + (n1 - n2));
+        Console.WriteLine("A multiplicação dos números digitados é: " + (n1 * n2));
+
+        if (n2 == 0)
+        {
+            Console.WriteLine("Impossível dividir por zero!");
+
+        }
+        else
+        {
+            Console.WriteLine("A divisão dos números é: " + (n1/n2));
+        }
+
+        Console.ReadKey();
+    }
+}
